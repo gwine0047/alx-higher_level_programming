81ed916fed7284dc2999b2c05ff5eb73302d88ca
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-08 17:59:39 +0100

    implement pycodestyle

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 result = ''
 
-for char in range(ord('z'), ord('a') -1, -1):
+for char in range(ord('z'), ord('a') - 1, -1):
     if char % 2 == 0:
         result += chr(char)
     else:
