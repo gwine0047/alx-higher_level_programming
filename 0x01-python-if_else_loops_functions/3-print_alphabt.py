ca44aeaa6c50f7a407e689b70d710690682b29de
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-08 17:07:05 +0100

    consult pycodestyle

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for alp in range(ord('a'), ord('z') + 1):
-    if alp == 'q' or alp == 'e':
-        alp = ''
-    print(chr(alp), end = ' ')
-
+    if chr(alp) != 'q' and chr(alp) != 'e':
+        print("{:c}".format(alp), end='')
