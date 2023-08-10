6f080ef4d82f37eef91695ded3dd8672e0e4b23c
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-10 19:21:31 +0100

    remove whitespaces

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, mul, sub, div
-import sys
+    import sys
 n = len(sys.argv) - 1
 if n != 3:
     print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
