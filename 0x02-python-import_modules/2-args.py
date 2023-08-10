d0e68f4ca194695558c1399e1a119d5e29f38357
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-10 19:27:53 +0100

    remove whitespaces

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ n = len(sys.argv)
 if n < 2:
     print("{:d} arguments.".format(n - 1))
 elif n == 2:
-    print("{:d} argument:\n{:d}: {:s}".format(n - 1, n, sys.argv[n - 1]))
+    print("{:d} argument:\n{:d}: {:s}".format((n - 1), (n - 1), sys.argv[n - 1]))
 else:
     print("{:d} arguments:".format(n - 1))
     for x in range(1, n):
