b6a51b0412f2b3c74750f4d60bc6c0bc8ac60dc2
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-10 15:14:01 +0100

    add 02-magic_calculation.py 103-fast_alphabet.py files

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from calculator_1 import add, mul, sub, div
+import sys
+n = len(sys.argv) - 1
+if n != 3:
+    print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
+    exit(1)
+else:
+    a = int(sys.argv[1])
+    operator = (sys.argv[2])
+    b = int(sys.argv[3])
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
+    elif operator == "/":
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print("{:d} {:s} {:d} = {}".format(a, operator, b, result))
