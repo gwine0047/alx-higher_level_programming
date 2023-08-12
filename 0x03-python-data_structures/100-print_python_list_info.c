5e28d203d0273e1626c0690b834fcc4662716951
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-12 20:02:40 +0100

    edit printf statement in 100-print_python_list_info.c file

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,4 @@
-#include <Python.h>
+#include "Python.h"
 
 /**
  * print_python_list_info - This function prints python list info
@@ -7,22 +7,21 @@
 void print_python_list_info(PyObject *p)
 {
 	Py_ssize_t i = 0, length;
-    PyListObject *object;
-    PyObject *token;
-    const char *token_type
+	PyListObject *object;
+	PyObject *token;
+	const char *token_type;
 
-    object (PyListObject *)p;
-    length = PyList_Size(p);
-    printf("[*] Size of the Python List = %d\n", (int)length);
-    printf("[*] Allocated = %d\n", (int)object->allocated)
+	object = (PyListObject *)p;
+	length = PyList_Size(p);
+	printf("[*] Size of the Python List = %d\n", (int)length);
+	printf("[*] Allocated = %d\n", (int)object->allocated)
 
-    while (i < length)
-    {
-        token = PyList_GetItem(p, i);
-        token_type = Py_TYPE(item)->tp_name;
-        printf("Element %d: %s\n", i, token_type);
-        i++;
-    }
-    
-
+	while (i < length)
+	{
+		token = PyList_GetItem(p, i);
+		token_type = Py_TYPE(item)->tp_name;
+		printf("Element %d: %s\n", i, token_type);
+		i++;
+	}
 }
+
