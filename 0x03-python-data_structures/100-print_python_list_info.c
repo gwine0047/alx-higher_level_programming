29141ca3a031eaa7aa836c4592d17d810c8bb813
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-12 19:01:02 +0100

    add 100-print_python_list_info.c file

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,7 @@
+#include <Python.h>
 #include <listobject.h>
 #include <object.h>
-#include <Python.h>
+
 /**
  * print_python_list_info - This function prints python list info
  * @p: is a pyobject
@@ -15,5 +16,5 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Size of the Python List = %li\n", length);
 	printf("[*] Allocated = %li\n", object->allocated)
 	for (i = 0; i < length; i++)
-		printf("Element %i: %s\n", i, Py_TYPE(object->ob_item[i]->tp_name))
+		printf("Element %li: %s\n", i, Py_TYPE(object->ob_item[i]->tp_name))
 }
