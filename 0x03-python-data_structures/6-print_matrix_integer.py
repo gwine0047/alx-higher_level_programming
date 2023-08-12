691d312fe8114907c0b874fa7105bab01f484f3e
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-12 17:29:07 +0100

    handle whitespaces and lack of newlines at the end of files

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for col in matrix:
         for element in col:
-            print("{:d}".format(element), end = " ")
+            print("{:d}".format(element), end=" ")
         print("")
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    first = tuple_a[ : 2] + (0, 0)
-    second = tuple_b[ : 2] + (0, 0)
+    first = tuple_a[: 2] + (0, 0)
+    second = tuple_b[: 2] + (0, 0)
 
     result = (first[0] + second[0], first[1] + second[1])
     return result
-
