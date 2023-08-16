c2bb8b76e37783e260e22469676641297ed982c9
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-16 19:17:12 +0100

    add 12-roman_to_int.py 11-multiply_list_map.py files

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or not roman_string:
+        return 0
+    
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    for numerals in reversed(roman_string):
+        num = romans[numerals]
+        total += num if total < num * 5 else -num
+    return total
---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,4 @@
+def print_sorted_dictionary(a_dictionary):
+    keys = a_dictionary.keys()
+    for key in sorted(keys):
+        print("{}: {}".format(key, a_dictionary[key]))
