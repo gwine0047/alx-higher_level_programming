02d58c93b3765a35d533bae7de29e872ed42a151
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-16 18:35:55 +0100

    add 3-common_elements.py file

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for element in set_1:
-        for item in set_2:
-            if element == item:
-                return element
+    return (set_1 & set_2)
