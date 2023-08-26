cd1ee9df2013194c04668a0bda85691b0de3b82d
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-26 13:09:07 +0100

    add path to file 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    squared = list(map((lambda a: list(map((lambda y: y * y), a))), matrix))
-    return squared
+    return list(map((lambda a: list(map((lambda y: y * y), a))), matrix))
