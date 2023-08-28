64c0d1d8ce2fb40c72cb0b98cf697c163312d2b3
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-28 17:22:15 +0100

    add 0-safe_print.py file

---
+++ 0x05-python-exceptions/0-safe_print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    result = 0
+    for idx in range(x):
+        try:
+            print(f"{my_list[idx]}", end="")
+            result += 1
+        except IndexError:
+            break
+    print()
+    return(result)
