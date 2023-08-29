68b4030c91cb8b18637b6975621529f5f3a646db
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-29 14:16:12 +0100

    add 4-square.py file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This is a square module"""
+
+
+class Square:
+    """square class declaration"""
+    def __init__(self, size=0):
+        """class attributes initialization
+        Args:
+            size: size of square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Attributes getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Public method of class Square"""
+        return self.__size * self.__size
