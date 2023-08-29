d45091555f438151456ae3ddf758c00196a4ba0c
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-29 18:20:03 +0100

    add 100-singlylinkedlist file

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -37,6 +37,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integer")
         elif value[1] < 0 or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integer")
+        self.__position = value
  
     def area(self):
         x = self.__size
