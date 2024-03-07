c40b2f2e10076aecccdd961b85142939f6675200
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-03-07 16:52:03 +0100

    remove whitespaces

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def sya_my_name(first_name, last_name=""):
+def say_my_name(first_name, last_name=""):
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
