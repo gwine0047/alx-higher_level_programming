56b895179a361d42d55a40780f3435731899994d
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-03-26 13:23:19 +0100

    add comment to file 2

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,5 @@
 -- scripts removes hbtn_0c_0 database from MySQL server.
 -- if database hbtn_0c_0  doesn't exist, script won't fail
+-- not allowed to use SELECT or SHOW
 
 DROP DATABASE IF EXISTS hbtn_0c_0;
