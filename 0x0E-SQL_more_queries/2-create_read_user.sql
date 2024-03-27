befc7121c79c8f67c4d004e9961d67414f1b1bfd
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-03-27 17:42:30 +0100

    change o to 0

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 -- user_0d_2 has only SELECT privilege in this database.
 -- the password is set touser_0d_2_pwd
 
-CREATE DATABASE IF NOT EXISTS hbtn_od_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
