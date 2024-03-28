b369d735924bad895981bf2f012f7c3519a649fb
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-03-28 19:30:54 +0100

    adjust code

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,7 +1,3 @@
 -- This script creates the table id_not_null on MySQL server.
--- id_not_null description:
---          id INT with default value = 1,
---          name VARCHAR(256).
--- if table force_name exists, script will not fail
 
 CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,4 @@
 -- this scripts lists all the California cities in the database hbtn_0d_usa.
--- The record display: cities.id, cities.name and states.name
--- sorted in ascending order by cities.id
--- use only SELECT statement
 
 SELECT cities.id, cities.name, states.name
 FROM cities
