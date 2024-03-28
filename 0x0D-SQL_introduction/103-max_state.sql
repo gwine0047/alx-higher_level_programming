1dda5168176cf2048556e9b86648ffcafaa8008f
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-03-28 18:04:12 +0100

    add semicolon

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,3 @@
 --displays the maximum temp of each state
---ordered by state name
 
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
