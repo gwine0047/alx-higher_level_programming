a7872f2f738ab34b150c7906205751c4c7ecd46b
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-03-28 12:51:20 +0100

    remove an unused line

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,3 @@
---Script creates hbtn_0c_0 as a database in MySQL server.
---If database exists, it won't create it.
+-- If database exists it won't create it.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
