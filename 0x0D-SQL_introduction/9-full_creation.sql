c03f60caf6644c0a35a95febf98ec88b5577b56a
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-03-28 13:28:03 +0100

    edit code in file 100

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- scripts creates table second in the database hbtn_0c_0
 
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), SCORE INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUES (1, "John", 10),
 (2, "Alex", 3)
