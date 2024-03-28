2dcbfce0db77d8283a8affdd6489e625ebfd5f94
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-03-28 10:01:53 +0100

    remove a line of code

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,10 @@
 -- this scripts lists all the California cities in the database hbtn_0d_usa.
 -- The record display: cities.id, cities.name and states.name
---sorted in ascending order by cities.id
+-- sorted in ascending order by cities.id
 -- use only SELECT statement
 
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.state_id=states.id
+INNER JOIN states
+ON cities.state_id=states.id
 ORDER BY cites.id;
