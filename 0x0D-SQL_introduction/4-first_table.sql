82c81e6c2fcc286f50ab65071e9e86d2db70a129
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-03-28 13:13:27 +0100

    edit code in file 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,3 @@
 -- This script creates a table called first_table in database
--- first_table description:
---      id INT
---      name VARCHAR(256)
--- if table exists, script won't fail
---no use of SELECT or SHOW
 
 CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
