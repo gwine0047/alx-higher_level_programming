ebe9c0addc15de84ad7a911d05adb13d012d7149
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-03-28 13:19:44 +0100

    edit code in file 10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,6 +1,3 @@
 --script will list all the records in the table: second_table
---display in this order, the score and the name
---ordered by the score
--- database name will be passed as an argument of the command
 
 SELECT score, name FROM second_table ORDER BY score DESC;
