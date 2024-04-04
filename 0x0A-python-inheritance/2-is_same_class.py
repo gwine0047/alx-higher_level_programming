fea5f5c8f75e71c36368c6818f2915cef162a19c
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-04-04 15:36:48 +0100

    use isinstance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,4 @@
 def is_same_class(obj, a_class):
     """Returns an true if obj is an instance, otherwise False"""
 
-    return (type(obj) == a_class)
+    return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
     """Returns true if directly or indirectly
     inheriting from a_class"""
 
-    return (issubclass(type(obj), a_class) and type(obj) != a_class)
+    return type(obj) != a_class and isinstance(obj, a_class)
