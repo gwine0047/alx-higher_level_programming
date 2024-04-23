7cbe88b6b80e27c0b4bdb9932f47dc2b6554cb2e
root <root@Okoeguale-Godwin>
2024-04-23 15:50:46 +0100

    done with mandatory tasks

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const Arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+Arr.forEach((element) => {
+  console.log(element);
+});
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const num = process.argv[2];
+
+if (!parseInt(num))
+{
+  console.log('Missing size');
+}
+else
+{
+  for (let i = 0; i < num; i++) {
+    let y = 0;
+    let myVar = '';
+
+    while (y < x) {
+      myVar = myVar + 'X';
+      y++;
+    }
+    console.log(myVar);
+  }
+}
